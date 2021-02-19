e7a55eac9918b88ad64aab442ad6b09d974d5529
Axel Tétart <axel.tetart@gmail.com>
2021-02-19 09:22:51 +0100

    Update TeasingGameHomeSceneController.cs
    Remove STSTransitionPrefab

--- Assets/Scripts/TeasingGameHomeSceneController.cs
+++ Assets/Scripts/TeasingGameHomeSceneController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using SceneTransitionSystem;
+//using SceneTransitionSystem;
 
 
 namespace TeasingGame
@@ -29,7 +29,7 @@ public class TeasingGameHomeSceneController : MonoBehaviour
 
    public void GoToGameScene()
     {
-        STSSceneManager.LoadScene(SceneForButton.ToString());
+        //STSSceneManager.LoadScene(SceneForButton.ToString());
     }
 }
 }
